909c2a9ea53a9e5350b28d568c0880a03337400b
Boris Amah <soft@SoftGuest.softguest.com>
2025-09-09 14:19:03 +0100

    updated whatsapp number

--- components/WhatsappButton.tsx
+++ components/WhatsappButton.tsx
@@ -5,7 +5,7 @@ import { MessageCircle } from "lucide-react";
 const WhatsappButton = () => {
   return (
     <a
-      href="https://wa.me/+237 683 165 717" // replace with your WhatsApp number (with country code, no + or spaces)
+      href="https://wa.me/237 678 078 246" // replace with your WhatsApp number (with country code, no + or spaces)
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-6 right-6 bg-green-500 hover:bg-green-600 text-white rounded-full p-4 shadow-lg flex items-center justify-center transition-transform transform hover:scale-110"
